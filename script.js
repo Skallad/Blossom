43209dbf4b0025afd047018ba476f5dac4619376
Skallad <gramatikofski.elena@gmail.com>
2022-09-10 16:02:19 +0000

    remove .js file, problem with finding file, restart script writing.

---
+++ script.js
@@ -0,0 +1,7 @@
+var tabs = docucment.querySelectorAll(".tabs ul li");
+var tabsWrap = document.querySelectorAll(".tab-wrap");
+
+tabs.forEach(function (tab, tabIndex){
+    console.log(tab);
+    console.log(tabIndex);
+})
