1125a57882652f84445e2abd33279693b7d477b6
Skallad <gramatikofski.elena@gmail.com>
2022-09-11 03:29:49 +0000

    add final JavaScript and styles, fix bugs and small errors

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ tabs.forEach(function (tab, tabIndex) {
     tab.addEventListener('click', function () {
 
         tabs.forEach(function (tab) {
-            tab.classList.remove('active')
+            tab.classList.remove('active');
         })
 
         tab.classList.add('active');
@@ -21,4 +21,24 @@ tabs.forEach(function (tab, tabIndex) {
     })
 })
 
+var monthTabs = document.querySelectorAll(".month-tabs ul li");
+var monthsWrap = document.querySelectorAll(".flower-container");
 
+monthTabs.forEach(function (month, monthIndex) {
+    month.addEventListener('click', function () {
+
+        monthTabs.forEach(function (month) {
+            month.classList.remove('active');
+        })
+
+        month.classList.add('active');
+
+        monthsWrap.forEach(function(flower, flowerIndex){
+            if(flowerIndex == monthIndex){
+                flower.style.display = "block"
+            } else{
+                flower.style.display = "none"
+            }
+        })
+    })
+})
